8a3d3d1f28471c8400514429f98a46e861dd22cf
lucio.si.ufs@gmail.com <lucio.si.ufs@gmail.com@ee77b906-a277-d99d-8edb-5c7d29cd67c9>
2013-08-18 19:49:53 +0000

    implementação caso de uso Produto

---
+++ trunk/Codigo/Codigo2/BibliotecaWeb/Controllers/ProdutoController.cs
@@ -0,0 +1,99 @@
+﻿using System.Web.Mvc;
+using Models.Models;
+using Services;
+
+
+namespace BibliotecaWeb.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        //
+        // GET: /Produto/
+
+        GerenciadorProduto gProduto;
+
+        public ProdutoController()
+        {
+            gProduto = new GerenciadorProduto();
+        }
+
+        public ActionResult Index()
+        {
+            return View(gProduto.ObterTodos());
+        }
+
+        //
+        // GET: /Produto/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View(gProduto.Obter(id));
+        }
+
+        //
+        // GET: /Produto/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Produto/Create
+
+        [HttpPost]
+        public ActionResult Create(Produto editoraModel)
+        {
+            if (ModelState.IsValid)
+            {
+                gProduto.Inserir(editoraModel);
+                return RedirectToAction("Index");
+            }
+
+            return View(editoraModel);
+        }
+        
+        //
+        // GET: /Produto/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Produto editoraModel = gProduto.Obter(id);
+            return View(editoraModel);
+        }
+
+        //
+        // POST: /Produto/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, Produto editoraModel)
+        {
+            if (ModelState.IsValid)
+            {
+                gProduto.Editar(editoraModel);
+                return RedirectToAction("Index");
+            }
+            return View(editoraModel);
+        }
+
+        //
+        // GET: /Produto/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Produto editoraModel = gProduto.Obter(id);
+            return View(editoraModel);
+        }
+
+        //
+        // POST: /Produto/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, Produto editoraModel)
+        {
+            gProduto.Remover(id);
+            return RedirectToAction("Index");
+            View();
+        }
+    }
+}
---
+++ trunk/Codigo/Codigo2/Model/Models/Modalidade.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using Models.App_GlobalResources;
+
+
+
+namespace Models.Models
+{
+    public class Modalidade
+    {
+        
+        [Display(Name = "Código")]
+        public int Codigo { get; set; }
+
+        [Required]
+        [StringLength(45)]
+        public string Nome { get; set; }
+
+
+        [Required(ErrorMessageResourceType = typeof(Resources), ErrorMessageResourceName = "erro_valor")]
+        [Range(0, 1000000)]
+        [DisplayFormat(DataFormatString = "{0:c}")]        
+        public decimal Valor { get; set; }
+
+    }
+}
