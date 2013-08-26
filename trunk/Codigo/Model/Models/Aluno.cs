ede77b4dbf2633c34033fc4d70b097c64cd4f754
islas95 <islas95@ee77b906-a277-d99d-8edb-5c7d29cd67c9>
2013-08-26 12:14:49 +0000

    Add Model Aluno e Pessoa

---
+++ trunk/Codigo/Model/Models/Aluno.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models.Models
+{
+    public class Aluno : Pessoa
+    {
+        public int Matricula { get; set; }
+        //public int CodigoPessoa { get; set; }
+        public decimal ValorMensalidade { get; set; }
+        public DateTime AtivoDesde { get; set; }
+        public DateTime DiaVencimento { get; set; }
+        public DateTime DataMatricula { get; set; }
+        public DateTime DataTrancamento { get; set; }    
+    }
+}
