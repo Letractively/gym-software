b2044b1dacedc1c9bf54700bef131a8c35094e9c
islas95 <islas95@ee77b906-a277-d99d-8edb-5c7d29cd67c9>
2013-08-26 12:14:49 +0000

    Add Model Aluno e Pessoa

---
+++ Codigo/Model/Models/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Models.Models
+{
+    public class Pessoa
+    {
+        public int CodigoPessoa { get; set; }
+        public DateTime CadastradoDesde { get; set; }
+        public string NomeRazao { get; set; }
+        public string ApelidoFantasia { get; set; }
+        public string CpfCnpj { get; set; }
+        public string RG { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public string Rua { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+        public int CEP { get; set; }
+        public string Complemento { get; set; }
+        public string Telefone1 { get; set; }
+        public string Telefone2 { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+       
+            
+            
+            
+    }
+}
