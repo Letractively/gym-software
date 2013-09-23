6f985fa39a3f30ccdfc07e65338b25c3ed0e2539
lucio.si.ufs@gmail.com <lucio.si.ufs@gmail.com@ee77b906-a277-d99d-8edb-5c7d29cd67c9>
2013-09-23 03:42:15 +0000

    criação de reports lista de produtis, fucionarios e funcionarios por permição

--- trunk/Codigo/BibliotecaWeb/Controllers/FuncionarioController.cs
+++ trunk/Codigo/BibliotecaWeb/Controllers/FuncionarioController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Models.Models;
 using Services;
+using Microsoft.Reporting.WebForms;
 
 namespace BibliotecaWeb.Controllers
 {
@@ -97,6 +98,91 @@ namespace BibliotecaWeb.Controllers
             gFuncionario.Remover(id);
             return RedirectToAction("Index");
             
+        }
+        public ActionResult RelatorioFuncionario()
+        {
+            LocalReport relatorio = new LocalReport();
+
+            //Caminho onde o arquivo do Report Viewer está localizado
+            relatorio.ReportPath = Server.MapPath("~/Reports/ReportListaFuncionario.rdlc");
+            //Define o nome do nosso DataSource e qual rotina irá preenche-lo, no caso, nosso método criado anteriormente
+            relatorio.DataSources.Add(new ReportDataSource("DataSetFuncionario", gFuncionario.ObterTodos()));
+
+            string reportType = "PDF";
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+            string deviceInfo =
+             "<DeviceInfo>" +
+             " <OutputFormat>PDF</OutputFormat>" +
+             " <PageWidth>9in</PageWidth>" +
+             " <PageHeight>11in</PageHeight>" +
+             " <MarginTop>0.7in</MarginTop>" +
+             " <MarginLeft>2in</MarginLeft>" +
+             " <MarginRight>2in</MarginRight>" +
+             " <MarginBottom>0.7in</MarginBottom>" +
+             "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] bytes;
+
+            //Renderiza o relatório em bytes
+            bytes = relatorio.Render(
+            reportType,
+            deviceInfo,
+            out mimeType,
+            out encoding,
+            out fileNameExtension,
+            out streams,
+            out warnings);
+
+            return File(bytes, mimeType);
+
+        }
+
+        public ActionResult RelatorioFuncionarioPorPermicao()
+        {
+            LocalReport relatorio = new LocalReport();
+
+            //Caminho onde o arquivo do Report Viewer está localizado
+            relatorio.ReportPath = Server.MapPath("~/Reports/ReportListaFuncionarioPorPermicao.rdlc");
+            //Define o nome do nosso DataSource e qual rotina irá preenche-lo, no caso, nosso método criado anteriormente
+            relatorio.DataSources.Add(new ReportDataSource("DataSetFuncionarioPorPermicao", gFuncionario.ObterTodos()));
+
+            string reportType = "PDF";
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+
+            string deviceInfo =
+             "<DeviceInfo>" +
+             " <OutputFormat>PDF</OutputFormat>" +
+             " <PageWidth>9in</PageWidth>" +
+             " <PageHeight>11in</PageHeight>" +
+             " <MarginTop>0.7in</MarginTop>" +
+             " <MarginLeft>2in</MarginLeft>" +
+             " <MarginRight>2in</MarginRight>" +
+             " <MarginBottom>0.7in</MarginBottom>" +
+             "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] bytes;
+
+            //Renderiza o relatório em bytes
+            bytes = relatorio.Render(
+            reportType,
+            deviceInfo,
+            out mimeType,
+            out encoding,
+            out fileNameExtension,
+            out streams,
+            out warnings);
+
+            return File(bytes, mimeType);
+
         }
     }
 }
