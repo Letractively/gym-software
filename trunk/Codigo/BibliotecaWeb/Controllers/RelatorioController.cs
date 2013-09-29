b9a8409822ea11163ac03d468c6fc2740e312b72
islas95 <islas95@ee77b906-a277-d99d-8edb-5c7d29cd67c9>
2013-09-29 22:46:19 +0000

    add RelatorioController.cs

---
+++ trunk/Codigo/BibliotecaWeb/Controllers/RelatorioController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Models.Models;
+using Services;
+
+namespace BibliotecaWeb.Controllers
+{
+    public class RelatorioController : Controller
+    {
+        
+        //
+        // GET: /Relatorio/
+       
+        public RelatorioController()
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+     
+
+    }
+}
